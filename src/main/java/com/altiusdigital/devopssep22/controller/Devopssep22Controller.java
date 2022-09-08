c92e3425973d3b4728631fa16bc75364632aac1e
Sankararaman Vedagiri <shnkar007@hotmail.com>
2022-09-08 18:49:35 +0530

    Sankar changes

---
+++ src/main/java/com/altiusdigital/devopssep22/controller/Devopssep22Controller.java
@@ -0,0 +1,14 @@
+package com.altiusdigital.devopssep22.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path="/devopssep22")
+public class Devopssep22Controller {
+    @GetMapping(path="/greet",produces="application/json")
+    public String greet(){
+        return "Hello";
+    }
+}
