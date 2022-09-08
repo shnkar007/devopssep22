56d90ad9d4aa3c20f2cbd5c65bff219b67127476
Sankararaman Vedagiri <shnkar007@hotmail.com>
2022-09-08 17:13:41 +0530

    project folders

---
+++ src/main/java/com/altiusdigital/devopssep22/Devopssep22Application.java
@@ -0,0 +1,13 @@
+package com.altiusdigital.devopssep22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devopssep22Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run (Devopssep22Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/altiusdigital/devopssep22/Devopssep22ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altiusdigital.devopssep22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Devopssep22ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
